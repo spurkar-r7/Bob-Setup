4a716770a8b6cac43daf3231dc50ee12bff480ff
Suhail Purkar <136419032+spurkar-r7@users.noreply.github.com>
2023-08-15 09:40:17 -0400

    Update bob_setup_windows.bat

--- bob_setup_windows.bat
+++ bob_setup_windows.bat
@@ -1,1 +1,75 @@
+@echo off
+echo "Hello! I am the Bob Installer 🤖 Please make sure to Install Chocolatey before running me so I can install the following applications onto your device 
 
+-Google Cloud
+-Git
+-Postgresql
+-Visual Studio Code
+-elasticsearch"
+
+
+echo "
+
+|||||||||||||||||||
+|                 |
+|  Installing Go  | 
+|                 |
+|||||||||||||||||||
+
+"
+choco install golang
+
+echo "
+
+|||||||||||||||||||||||||||||
+|                           |
+|  Installing Google Cloud  | 
+|                           |
+|||||||||||||||||||||||||||||
+
+"
+choco install gcloudsdk
+
+echo "
+
+||||||||||||||||||||
+|                  |
+|  Installing Git  | 
+|                  |
+||||||||||||||||||||
+
+"
+choco install git.install
+
+echo "
+
+|||||||||||||||||||||||||
+|                       |
+|  Installing Postgres  | 
+|                       |
+|||||||||||||||||||||||||
+
+"
+choco install postgresql14
+
+echo "
+
+||||||||||||||||||||||||||||||||||
+|                                |
+|  Installing Visual Studio Code | 
+|                                |
+||||||||||||||||||||||||||||||||||
+
+"
+choco install vscode
+
+echo "
+
+||||||||||||||||||||||||||||||
+|                            |
+|  Installing Elasticsearch  | 
+|                            |
+||||||||||||||||||||||||||||||
+
+"
+choco install elasticsearch
